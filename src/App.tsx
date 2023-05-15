612f63f84c3bc2aa05c46aa00c382df78538ebf5
JagsP23 <pullapuramjr@gmail.com>
2023-05-15 14:52:12 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import { produce } from "immer";
+
+
+
+const App = () => {
+
+  const [cartItems, setCartItems] = useState(['product 1' , 'product 2' , 'product 3'])
+  
+  const [bugs,setBugs]=useState([
+    {id:1, title:'Bug 1', fixed:false},
+    {id:2, title:'Bug 2', fixed:false},
+  ]);
+
+  const handleClick = () =>{
+    
+    //setBugs(bugs.map(bug => bug.id === 1 ? {...bugs, fixed: true} : bugs));
+    setBugs(produce(draft => {
+      const bug = draft.find(bug => bug.id === 1);
+      if(bug) bug.fixed=true;
+    }))
+  }
+
+
+
+  return (
+    <div onClick={handleClick}>
+      {bugs.map(bug => <p key={bug.id}>{bug.title} {bug.fixed ? "fixed" : "New"}</p>)}
+      <button>Click Me</button>
+    </div>
+  )
+}
+
+export default App
